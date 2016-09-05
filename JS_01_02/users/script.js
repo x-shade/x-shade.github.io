72715d4e80771d23ef08806533cc7f7a9c2e101e
x-shade <dasha-zh@mail.ru>
2016-09-06 02:03:18 +0300

    uploaded js 01-02

---
+++ JS_01_02/users/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+    var arr = [];
+    arr.length = 5;
+    
+    function fillArray() {
+        var i;
+        
+        for (i = 0; i < arr.length; ++i) {
+        arr[i] = prompt('Введите имя пользователя ' + (i + 1), '');
+      }
+        
+    console.log(arr);
+    }
+    
+    fillArray();
+    
+    var name = prompt('Введите Ваше имя', '');
+    console.log(name);
+    
+    function hello(name) {
+        var check = false;
+        
+        for (var k = 0; k < arr.length; ++k) {
+            if (name == arr[k]) {
+                check = true;
+            }
+        }
+        
+        if (check == true) {
+            alert(name + ', Вы успешно вошли');
+            console.log(name + ', Вы успешно вошли');
+        } else {
+            alert('Пользователя с таким именем нет');
+            console.log('Ошибка, пользователя с таким именем нет');
+        }
+    }
+    
+    hello(name);
+}());
